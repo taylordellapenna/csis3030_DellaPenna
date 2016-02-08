315c1673b2392778db72d577241d9e8bf1452bc3
Taylor DellaPenna <tld271@wildcats.jwu.edu>
2016-02-08 12:39:09 -0500

    Ecommerce Site

---
+++ global.php
@@ -0,0 +1,20 @@
+<?php
+
+$connection = mysqli_connect("localhost","root","ecommerce",);
+session_start();
+
+/*
+	create table register (
+		id int auto_increment not null primary key,
+		email varchar(200) not null,
+		password varchar(200) not null,
+		firstname varchar(200) not null,
+		lastname varchar(200) not null,
+		passwordconfirm varchar(200) not null);
+
+
+
+*/
+
+
+?> 
---
+++ part1.sql
@@ -0,0 +1,17 @@
+INSERT INTO categories (category_name)
+VALUES ('Books');
+
+INSERT INTO categories (category_name)
+VALUES ('E-Readers');
+
+INSERT INTO products (category_id,product_name,price,quantity_remaining,description,image)
+VALUES ('1','A Doubter\'s Almanac','$16.80','1 New','Milo Andret is born with an unusual mind. A lonely child growing up in the woods of northern Michigan in the 1950s, he gives little thought to his own talent. But with his acceptance at U.C. Berkeley he realizes the extent, and the risks, of his singular gifts','book_1.jpg' );
+
+INSERT INTO products (category_id,product_name,price,quantity_remaining,description,image)
+VALUES ('1','The Girl in the Red Coat','$15.57','1 New','Newly single mom Beth has one constant, gnawing worry: that her dreamy eight-year-old daughter, Carmel, who has a tendency to wander off, will one day go missing.','book_2.jpg' );
+
+INSERT INTO products (category_id,product_name,price,quantity_remaining,description,image)
+VALUES ('2','Ghost Gifts','$5.99','None','All Aubrey Ellis wants is a normal life, one that doesn’t include desperate pleas from the dead. Her remarkable gift may help others rest in peace, but it also made for an unsettling childhood and destroyed her marriage.','book_3.jpg' );
+
+INSERT INTO products (category_id,product_name,price,quantity_remaining,description,image)
+VALUES ('2','North of Here','$5.99','None','The sounds of unexpected tragedies—a roll of thunder, the crash of metal on metal—leave Miranda in shock amid the ruins of her broken family.','book_4.jpg' );
